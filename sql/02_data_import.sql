159b9dc19d10838797af48eddd03835a8bb5436f
Anjali Rawat <rawatanjali0513@gmail.com>
2026-08-04 13:36:16 +0530

    Update 02_data_import.sql

--- sql/02_data_import.sql
+++ sql/02_data_import.sql
@@ -1,1 +1,24 @@
+-- =====================================================
+-- Loan Rejection Funnel Analysis
+-- File: 02_data_import.sql
+-- Description: Import loan approval dataset
+-- =====================================================
 
+-- Import data using pgAdmin Import/Export Wizard
+-- OR use COPY if required
+
+/*
+COPY loan_applications
+FROM 'C:\Users\YourName\Downloads\loan_approval_dataset.csv'
+DELIMITER ','
+CSV HEADER;
+*/
+
+-- Verify import
+SELECT COUNT(*) AS total_records
+FROM loan_applications;
+
+-- Preview data
+SELECT *
+FROM loan_applications
+LIMIT 10;
